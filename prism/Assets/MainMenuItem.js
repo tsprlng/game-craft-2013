2c58cf82537a4b3cbb3522a3234c33e7538fb0be
Jim O'Brien <jimobrien@firmstep.com>
2013-08-10 14:28:21 +0100

    Add main menu scene and script

---
+++ prism/Assets/MainMenuItem.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+var levelName : String;
+var quitOnClick : boolean;
+
+function OnMouseEnter() {
+	renderer.material.color = Color.yellow;
+}
+
+function OnMouseExit() {
+	renderer.material.color = Color.white;
+}
+
+function OnMouseUp() {
+	if(quitOnClick) {
+		Application.Quit();
+	} else {
+		Application.LoadLevel(levelName);
+	}
+}
