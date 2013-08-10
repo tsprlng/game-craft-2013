111843f63b9e14e452191518536d550b567c8694
Tom Spurling <tom@tspurling.co.uk>
2013-08-10 18:40:24 +0100

    Now we have a police builiding and some camera

---
+++ prism/Assets/CameraLook.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLook : MonoBehaviour {
+	
+	private GameObject target;
+	
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(target.transform);
+	}
+}
